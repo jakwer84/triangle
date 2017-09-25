3426386705fde9e534f6812d49ef7b82d324d09b
Jakub Wereszczynski <jakwer@gmail.com>
2017-09-25 18:06:05 +0200

    triangle_excercise

---
+++ script.js
@@ -0,0 +1,7 @@
+	var
+		h = 12;
+	var triangleArea = a*h/2;
+	console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	function myFunction() {
+    alert('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+	}
